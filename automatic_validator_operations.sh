f4d9a834268d800fa4cfd709ef894253b33860fe
Vanlee-S <71849886+Vanlee-S@users.noreply.github.com>
2021-01-20 12:11:51 +0300

    Update automatic_validator_operations.sh

--- automatic_validator_operations.sh
+++ automatic_validator_operations.sh
@@ -5,15 +5,14 @@ echo "Crypto.com Automatic Validator Operations script by Christian Vari"
 if [ "$#" == 0 ]
 then
     echo "Please run the script as:"
-    echo "./automatic_validator_operations.sh <delegatorAddress> <operatorAddress> <keyPassword> <keyring> <node>"
+    echo "./automatic_validator_operations.sh <delegatorAddress> <operatorAddress> <keyPassword> <node>"
     exit 0
 fi
 
 delegatorAddress=$1
 operatorAddress=$2
 keyPassword=$3
-keyring=$4
-node=$5
+node=$4
 while [ true ]
 do
     currentBalance=`./chain-maind query bank balances $delegatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
@@ -23,12 +22,12 @@ do
     if (( $(echo "$currentAvailableReward > 10000000000" |bc -l) )) 
     then
             echo "Withdrawing rewards..."
-            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $operatorAddress --commission --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --keyring-backend="file" --node $node  -y
+            echo $keyPassword | ./chain-maind tx distribution withdraw-rewards $operatorAddress --commission --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
     fi
     if (( $(echo "$currentBalance > 10000000000" |bc -l) )) 
     then
             echo "Delegating rewards..."
-            echo $keyPassword | ./chain-maind tx staking delegate $operatorAddress "$currentBalance"basetcro --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --keyring-backend="file" --node $node  -y
+            echo $keyPassword | ./chain-maind tx staking delegate $operatorAddress "$currentBalance"basetcro --from $keyring --gas 80000000 --gas-prices 0.1basetcro --chain-id="crossfire" --node $node  -y
     fi
     sleep 5m
 done
