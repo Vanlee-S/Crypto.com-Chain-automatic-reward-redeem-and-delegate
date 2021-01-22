75a4ec0109e44fb1ab65fdf522cadd4a3520da38
Vanlee-S <71849886+Vanlee-S@users.noreply.github.com>
2021-01-22 11:23:54 +0300

    Update automatic_validator_operations.sh

--- automatic_validator_operations.sh
+++ automatic_validator_operations.sh
@@ -18,7 +18,7 @@ while [ true ]
 do
     currentBalance=`./chain-maind query bank balances $delegatorAddress --output=json --node $node | jq -r ".balances[0].amount"`
     echo "Current balance: $currentBalance"
-    currentAvailableReward=`./chain-maind query distribution rewards $delegatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
+    currentAvailableReward=`./chain-maind q distribution validator-outstanding-rewards $operatorAddress --output=json --node $node  | jq -r ".total[0].amount"`
     echo "Current Available Delegator Rewards: $currentAvailableReward"
     if (( $(echo "$currentAvailableReward > 10000000000" |bc -l) )) 
     then
